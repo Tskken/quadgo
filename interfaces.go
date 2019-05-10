45db79915dcc6bd8dff855ccc1193bc4504114ad
Tsukinai <dylan.blanchard@mymail.champlain.edu>
2019-05-09 23:53:32 -0600

    V1.1.1
    Fixed small error in Entity documentation.

--- interfaces.go
+++ interfaces.go
@@ -16,8 +16,8 @@ type Bounder interface {
 // Any entity you want to add to the quadtree must implement this interface.
 //
 // Note: This currently is exactly the same as Bounder but may change in the future if
-// 		I isEntity any need for Entity specific functions. For know it is just hear to make reading
-//		the code easier as having Bounder in areas were you were dealing with entities seemed odd.
+// 	I find any need for Entity specific functions. For know it is just hear to make reading
+//	the code easier as having Bounder in areas were you were dealing with entities seemed odd.
 type Entity interface {
 	Bounder
 }
